b40eb1d9d499670584bd53b6f55d4507b8fc1a3a
Logesh Paramalai <125880059+Logi257@users.noreply.github.com>
2024-12-15 10:35:12 +0530

    Add files via upload

---
+++ ToDoList/script.js
@@ -0,0 +1,46 @@
+const inputBox=document.getElementById('input-box');
+const listContainer=document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let deleteIcon = document.createElement("i");
+        deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
+        li.appendChild(deleteIcon);
+
+
+    }
+    inputBox.value='';
+    saveData()
+
+
+}
+
+listContainer.addEventListener("click",function(e){
+    if (e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+
+    }
+    else if(e.target.tagName==="I"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+
+
+
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+ 
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask()
